d7d66d7e24a4f7057d7484d13aa746f6d1933e8e
ImamAlBukhori <imamxfire@gmail.com>
2025-05-25 15:22:45 +0700

    Pertemuan 6 : Fungsi

--- pertemuan 6/belajarFungsi.cpp
+++ pertemuan 6/belajarFungsi.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama) { //parameter formal
+void sapa(string nama) { //parameter formal, Variabel yang dideklarasikan di definisi fungsi, sebagai tempat menerima nilai input saat fungsi dipanggil.
     cout << "Halo " << nama <<"! Selamat Datang di AP 2 !" << endl;
 }
 
 int main () {
     string namaPengguna = "Imam";
 
-    sapa(namaPengguna); //parameter aktual
+    sapa(namaPengguna); //parameter aktual, Nilai atau variabel yang dikirim ke fungsi saat dipanggil, untuk menggantikan parameter formal.
 
     return 0;
 }
