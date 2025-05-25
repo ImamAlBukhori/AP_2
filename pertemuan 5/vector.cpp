dba4c0dbb9afd79f7906e3bf718b1e385e93873d
ImamAlBukhori <imamxfire@gmail.com>
2025-05-25 15:20:09 +0700

    Pertemuan 5 : Array & Vector

--- pertemuan 5/vector.cpp
+++ pertemuan 5/vector.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main () {
-    // Vector Declaration & Initialization
+    // Vector Declaration & Initialization, array dinamis yang bisa menambah atau menghapus elemen secara otomatis.
     vector<string> nama_karyawan = {"nolan", "julian", "lukas", "lancelot"};
 
-    // Ascending Element of Vector
+    // Ascending Element of Vector, Mengurutkan elemen-elemen dalam vector dari nilai terkecil ke terbesar
     // for (int i=0; i < nama_karyawan.size(); i++){
     //     cout << nama_karyawan[i] << endl;
     // }
@@ -15,13 +15,13 @@ int main () {
     //     cout << karyawan << endl;
     // }
 
-    // Add data to vector
+    // Add data to vector, menambahkan data ke vector
     nama_karyawan.push_back("imam");
     // for (int i=0; i < nama_karyawan.size(); i++){
     //     cout << nama_karyawan[i] << endl;
     // }
 
-    // Delete data from vector
+    // Delete data from vector, menghapus data dari vector.
     nama_karyawan.pop_back();
     nama_karyawan.erase(nama_karyawan.begin() + 3);
     
